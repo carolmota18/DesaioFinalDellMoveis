aeaebc8a3325e5de96b180991fa419fccaa64f68
caroline mota <carolinemota78@gmail.com>
2023-05-21 03:10:50 +0000

    Update 3 files

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -4,25 +4,51 @@ import Logo from "../Imagens/logodellmovies.png";
 import Pesquisa from "../Imagens/pesquisa.png";
 
 const Conteiner = styled.div`
+  display: flex;
+  justify-content: space-around;
   background: transparent;
   opacity: 1;
   backdrop-filter: blur(9px);
   -webkit-backdrop-filter: blur(9px);
+  li {
+    font-family: "Open Sans", sans-serif;
+  }
+`;
+
+const ListaUm = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  list-style: none;
+`;
+
+const Icone = styled.img`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const ListaDois = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 20px;
+  list-style: none;
 `;
 
 export default function Header() {
   return (
     <Conteiner>
-      <img src={Logo} alt="logo" />
-      <ul>
+      <Icone src={Logo} alt="logo" />
+      <ListaUm>
         <li>Séries</li>
         <li>Filmes</li>
-      </ul>
+      </ListaUm>
       <img src={Pesquisa} alt="pesquisa" />
-      <ul>
+      <ListaDois>
         <li>Filtro</li>
         <li>Login</li>
-      </ul>
+      </ListaDois>
     </Conteiner>
   );
 }
